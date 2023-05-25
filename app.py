f5b60c3f2127ad467b078d5a148ceab09eca9811
Kevin Kibe <kchegz234@gmail.com>
2023-05-25 19:51:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -71,21 +71,14 @@ def make_prediction(feature_values):
             break
     return prediction_range
 st.title("Data Professions Salary Estimation")
-input_features = {
-    'remote_ratio': None,
-    'experience_level': None,
-    'employment_type': None,
-    'job_title': None,
-    'year': 2023,
-    'company_size': None
-}
+input_features = {}
+
 input_features['remote_ratio'] = st.selectbox('Remote Ratio: 0 for No Remote Work, 50 for Hybrid Setup, 100 for Fully Remote"', options=[0, 50, 100], key='remote_ratio')
 input_features['experience_level'] = st.selectbox('Experience Level:', options=[('EN','Entry-level / Junior'),('MI','Mid-level / Intermediate'),('SE','Senior-level / Expert'),('EX','Executive-level / Director')], format_func=lambda x: x[1], key='experience_level')
-input_features['employment_type'] = st.selectbox('Employment Type:', options=[('PT','Part-time'),('FT','Full-time'),('CT','Contract'),('FL','Freelance')], format_func=lambda x: x[1],  key='employment_type')
+input_features['employment_type'] = st.selectbox('Employment Type:', options=[('PT','Part-time'),('FT','Full-time'),('CT','Contract'),('FL','Freelance')], format_func=lambda x: x[1], key='employment_type')
 input_features['job_title'] = st.selectbox('Job Title:', options=['data analyst','data engineer','data scientist','machine learning engineer'], key='job_title')
+input_features['year'] = 2023
 input_features['company_size'] = st.selectbox('Company Size:', options=[('S','less than 50 employees (small)'),('M','50 to 250 employees (medium)'),('L','more than 250 employees (large)')], format_func=lambda x: x[1], key='company_size')
 
-
-if st.button('Salary Estimation'):
-    prediction = make_prediction(input_features)
-    st.write("Estimation in USD :", prediction)
+prediction = make_prediction(input_features)
+st.write("Estimation in USD :", prediction)
