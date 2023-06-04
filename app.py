1b62bc79e0d5e0764dae4ecaef533d02144bcf48
Kevin Kibe <kchegz234@gmail.com>
2023-06-04 11:45:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,13 @@ def load_model():
     return model
 model=load_model()
 def make_prediction(feature_values):
-    feature_names = ['remote_ratio',
+    feature_names = ['employee_residence_Africa',
+                     'employee_residence_Asia', 'employee_residence_Europe',
+                     'employee_residence_North America', 'employee_residence_Oceania',
+                     'employee_residence_South America', 'company_location_Africa',
+                     'company_location_Asia', 'company_location_Europe',
+                     'company_location_North America', 'company_location_Oceania',
+                     'company_location_South America',
                      'experience_level_EN', 
                      'experience_level_EX', 
                      'experience_level_MI', 
@@ -34,7 +40,18 @@ def make_prediction(feature_values):
                      'year_2022',
                      'year_2023']
     # Create a numpy array for the input data
-    input_data = np.array([[feature_values['remote_ratio'] ,
+    input_data = np.array([[feature_values['employee_residence'] == 'Africa',
+                            feature_values['employee_residence'] == 'Asia',
+                            feature_values['employee_residence'] == 'Europe',
+                            feature_values['employee_residence'] == 'North America',
+                            feature_values['employee_residence'] == 'Oceania',
+                            feature_values['employee_residence'] == 'South America',
+                            feature_values['company_location'] == 'Africa',
+                            feature_values['company_location'] == 'Asia',
+                            feature_values['company_location'] == 'Europe',
+                            feature_values['company_location'] == 'North America',
+                            feature_values['company_location'] == 'Oceania',
+                            feature_values['company_location'] == 'South America',
                             feature_values['experience_level'] == 'EN',
                             feature_values['experience_level'] == 'EX',
                             feature_values['experience_level'] == 'MI',
@@ -55,14 +72,21 @@ def make_prediction(feature_values):
                             feature_values['year'] == 2022,
                             feature_values['year'] == 2023]])
     prediction = model.predict(input_data)
-    ranges = [(15000 , 48875), 
-              (48875 , 82750), 
-              (82750 , 116625), 
-              (116625 , 150500), 
-              (150500 , 184375),
-              (184375 , 218250), 
-              (218250 , 252125), 
-              (252125 , 286000)]
+    ranges = [(15000 - 33066),
+              (33066 - 51133),
+              (51133 - 69200),
+              (69200 - 87266),
+              (87266 - 105333),
+              (105333 - 123400),
+              (123400 - 141466),
+              (141466 - 159533),
+              (159533 - 177600),
+              (177600 - 195666),
+              (195666 - 213733),
+              (213733 - 231800),
+              (231800 - 249866),
+              (249866 - 267933),
+              (267933 - 286000)]
     prediction_range = None
     for range_min, range_max in ranges:
         if range_min <= prediction < range_max:
@@ -78,6 +102,8 @@ input_features['employment_type'] = st.selectbox('Employment Type:', options=[('
 input_features['job_title'] = st.selectbox('Job Title:', options=['data analyst','data engineer','data scientist','machine learning engineer'], key='job_title')
 input_features['year'] = 2023
 input_features['company_size'] = st.selectbox('Company Size:', options=[('S','less than 50 employees (small)'),('M','50 to 250 employees (medium)'),('L','more than 250 employees (large)')], format_func=lambda x: x[1], key='company_size')
+input_features['employee_residence'] = st.selectbox('Employee Residence:', options=[('Africa','Africa'),('Asia','Asia'),('Europe','Europe'),('North America','North America'),('Oceania','Oceania'),('South America','South America')], format_func=lambda x: x[1], key='employee_residence')
+input_features['company_location'] = st.selectbox(Company Location:', options=[('Africa','Africa'),('Asia','Asia'),('Europe','Europe'),('North America','North America'),('Oceania','Oceania'),('South America','South America')], format_func=lambda x: x[1], key='company_location')
 
 if st.button('Estimate'):
     prediction = make_prediction(input_features)
